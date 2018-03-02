6595a57ce894bf63fe57498efac45697d433a42c
ilaytzu <ilay.tzu@gmail.com>
2018-03-03 00:32:43 +0200

    added doc

--- unu.py
+++ unu.py
@@ -4,24 +4,27 @@ colors = ("red", "yellow", "green", "blue")
 
 
 class Card:
+    """the class object for the unu game"""
     def __init__(self, num, color):
+        """initiate the card"""
         self.num = num
         self.color = color
 
     def print_card(self):
+        """print the card"""
         print(self.num, self.color)
 
 
 def generate_hand(card_num):
+    """returns a list containing a specified amount of cards"""
     h = []
     for x in range(card_num):
-        color = random.choice(colors)
-        num = random.randint(1, 8)
-        h.append(Card(num, color))
+        h.append(Card(random.randint(1, 8), random.choice(colors)))
     return h
 
 
 def show_hand(h):
+    """prints a hand"""
     for x in h:
         x.print_card()
 
